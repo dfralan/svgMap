65bff54cfd6b22e9a3372d2c91850a3390ba4947
dfralan <80047722+dfralan@users.noreply.github.com>
2022-01-20 11:56:48 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+//scroller
+document.getElementById("button1").addEventListener("click", function() {
+  document.getElementById("here1").scrollIntoView()
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+  document.getElementById("here2").scrollIntoView()
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+  document.getElementById("here3").scrollIntoView()
+});
+document.getElementById("button4").addEventListener("click", function() {
+  document.getElementById("here4").scrollIntoView()
+});
+
+document.getElementById("button6").addEventListener("click", function() {
+  document.getElementById("here6").scrollIntoView()
+});
+
+document.getElementById("button5").addEventListener("click", function() {
+  document.getElementById("here5").scrollIntoView()
+});
